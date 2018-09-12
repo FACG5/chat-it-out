dc51e0d70ec007d0f6f3ba3a3750d59ac68980aa
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-09-12 14:23:19 +0300

    Final Edits

--- src/app.js
+++ src/app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+// Middlewares
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 
 module.exports = app;
