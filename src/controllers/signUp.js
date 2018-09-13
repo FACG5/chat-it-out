3bf51f5608b143ea001ef420f162ff613724ecf7
Ons Nader <onsmariposa-nana@hotmail.com>
2018-09-13 12:29:46 +0300

    linked js files Relates #47

--- src/controllers/signUp.js
+++ src/controllers/signUp.js
@@ -1,5 +1,6 @@
 exports.get = (req,res) => {
     res.render("sign-up", {
-        title: "Sign-up", headerFound: false, footerFound: false, asideFound:false, style: ["signUpIn"]
+        title: "Sign-up", headerFound: false, footerFound: false, asideFound:false, style: ["signUpIn"],
+         javascript: ['sign-up']
     });
 };
