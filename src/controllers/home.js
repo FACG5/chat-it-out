7fbdb9f1ee11f9be434fc0e37e0de5429d28b8c1
Ons Nader <onsmariposa-nana@hotmail.com>
2018-09-13 11:43:13 +0300

    resolved issues

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,6 @@
 exports.get = (req, res) => {
     res.render("home", {
-        title: "chat it out", headerFound: true, footerFound: true, style: ["home", "footer", "header"],
-        javascript: ['home']
+        title: "chat it out", headerFound: true, footerFound: true, style: ["home", "footer", "header", "public"],
+        javascript: ['home', 'hamburger']
     });
 };
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,11 @@
 const Router = require('express').Router();
 const chat = require('./chat');
-
+const home = require('./home');
+const signUp = require ('./signUp');
+const signIn = require ('./signIn');
+Router.get("/", home.get);
+Router.get("/signUp", signUp.get);
+Router.get('/signIn', signIn.get);
 Router.get('/chat', chat.get);
 
 module.exports = Router;
