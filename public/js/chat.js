3e50a3e4d62f79c64c4bb88d3ed228b757dbe8fc
knanahassouna1 <knanahassouna2@gmail.com>
2018-09-13 21:42:55 +0300

    solve issues Relates #39

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,38 +1,38 @@
 
-const header = document.getElementById('header');
-const messages = document.getElementById('messages');
-const messenger_space = document.getElementById('messenger_space');
-const last_messages = document.getElementById('last_messages');
-const back_btn = document.getElementById('back_btn');
+const headerTag = document.getElementById('header');
+const messagesTag = document.getElementById('messages');
+const messengerSpaceTag = document.getElementById('messenger_space');
+const lastMessagesTag = document.getElementById('last_messages');
+const backBtn = document.getElementById('back_btn');
+const alterHeaderTag = document.getElementById('alter_header');
 
-messages.addEventListener('click', (event) => {
-
+messagesTag.addEventListener('click', (event) => {
   if (window.innerWidth < 750 && event.target.tagName !== 'UL') {
-    headerVisible('none', 'block', 'none', 'flex');
+    changeHeaderVisibility('none', 'block', 'none', 'flex');
   }
-
 });
-back_btn.addEventListener('click', () => {
 
+backBtn.addEventListener('click', () => {
   if (window.innerWidth < 750) {
-    headerVisible('block', 'none', 'flex', 'none');
+    changeHeaderVisibility('block', 'none', 'flex', 'none');
   }
-
 });
+
 window.addEventListener('resize', () => {
-
   if (window.innerWidth > 750) {
-    headerVisible('block', 'block', 'flex', 'none');
+    changeHeaderVisibility('block', 'block', 'flex', 'none');
   } else {
-    last_messages.setAttribute('style', 'display:block');
-    messenger_space.setAttribute('style', 'display:none');
-    header.setAttribute('style', 'display:flex');
+    changeHeaderVisibility('block', 'none', 'flex');
+  }
+});
+
+function changeHeaderVisibility(lastMessages, messengerSpace, header, alterHeader) {
+
+  lastMessagesTag.setAttribute('style', `display:${lastMessages};`);
+  messengerSpaceTag.setAttribute('style', `display:${messengerSpace};`);
+  headerTag.setAttribute('style', `display:${header};`);
+  if (alterHeader) {
+    alterHeaderTag.setAttribute('style', `display:${alterHeader};`);
   }
 
-});
-const headerVisible = (last_messages, messenger_space, header, alter_header) => {
-  last_messages.setAttribute('style', `display:${last_messages};`);
-  messenger_space.setAttribute('style', `display:${messenger_space};`);
-  header.setAttribute('style', `display:${header};`);
-  alter_header.setAttribute('style', `display:${alter_header};`);
 };
--- test/test_BE.js
+++ test/test_BE.js
@@ -4,17 +4,17 @@ const app = require('./../src/app');
 
 
 // Test Doctors Route
-tape('check doctor.get route ', (t) => {
+tape('Check /chat Route', (t) => {
   supertest(app)
     .get('/chat')
     .expect(200)
+    .expect('Content-type', /html/)
     .end((err, res) => {
       if (err) {
         t.error(err);
       }
       t.ok(res.text.includes('body'));
       t.equal(res.text.substr(0, 15), '<!DOCTYPE html>', 'the respone  should be html file');
-      t.equal(res.header['content-type'], 'text/html; charset=utf-8', 'the content-type should equal html');
       t.end();
     });
 });
