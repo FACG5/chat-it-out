30791a60e6c5e9ad84bda1f5113ddac0351864b7
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-09-14 19:57:31 +0300

    backup

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,5 +1,5 @@
 exports.get = (req, res) => {
   res.render('admin', {
-    title: 'Admin Panel', headerFound: true, style: ['header', 'admin'], javascript: ['hamburger', 'admin'],
+    title: 'Admin Panel', headerFound: true, style: ['admin', 'header'], javascript: ['hamburger', 'admin'],
   });
 };
