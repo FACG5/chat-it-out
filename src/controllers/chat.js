18e27707ecc5266615dfc83a2f28cdc54df6d9a8
knanahassouna1 <knanahassouna2@gmail.com>
2018-09-15 22:34:12 +0300

    remove unrelated issues #39

--- src/controllers/chat.js
+++
@@ -1,5 +0,0 @@
-exports.get = (req, res) => {
-  res.render('chat', {
-    title: 'Messenger Page || Chat It Out', headerFound: true, footerFound: false, style: ['chat', 'header'], javascript: ['chat', 'hamburger_menu'],
-  });
-};
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,6 @@
 const Router = require('express').Router();
-const chat = require('./chat');
 const doctors = require('./doctors');
 
-Router.get('/chat', chat.get);
 Router.get('/doctors', doctors.get);
 
 module.exports = Router;
