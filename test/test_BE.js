ad83208f0f8be555424deb52ba0aa9de10d1c35d
Ons Nader <onsmariposa-nana@hotmail.com>
2018-09-15 11:24:45 +0300

    resolved some issues

--- test/test_BE.js
+++ test/test_BE.js
@@ -6,6 +6,7 @@ tape('test for home route ', (t) => {
     supertest(app)
       .get('/')
       .expect(200)
+      .expect('Content-Type', /html/)
       .end((err, res) => {
         if (err) {
           t.error(err);
@@ -14,35 +15,7 @@ tape('test for home route ', (t) => {
         t.equal(res.text.substr(0, 15), '<!DOCTYPE html>', 'the respone  should be html file');
         t.equal(res.header['content-type'], 'text/html; charset=utf-8', 'the content-type should equal html');
         t.end();
+
       });
   });
-  //test the sign-in route
-  tape('test for sign-up', (t) => {
-    supertest(app)
-      .get('/signIn')
-      .expect(200)
-      .end((err, res) => {
-        if (err) {
-          t.error(err);
-        }
-        t.ok(res.text.includes('body'));
-        t.equal(res.text.substr(0, 15), '<!DOCTYPE html>', 'the respone  should be html file');
-        t.equal(res.header['content-type'], 'text/html; charset=utf-8', 'the content-type should equal html');
-        t.end();
-      });
-  });
-  //test the sign-up route
-  tape('test for sign-up', (t) => {
-    supertest(app)
-      .get('/signUp')
-      .expect(200)
-      .end((err, res) => {
-        if (err) {
-          t.error(err);
-        }
-        t.ok(res.text.includes('body'));
-        t.equal(res.text.substr(0, 15), '<!DOCTYPE html>', 'the respone  should be html file');
-        t.equal(res.header['content-type'], 'text/html; charset=utf-8', 'the content-type should equal html');
-        t.end();
-      });
-  });
+  
