6d2531c950c5ca74ce9dcdf8bba847cbd126519c
knanahassouna1 <knanahassouna2@gmail.com>
2018-09-16 16:58:09 +0300

    solve all issues #44

--- test/test_BE.js
+++ test/test_BE.js
@@ -1,51 +1,50 @@
+const tape = require('tape');
+const supertest = require('supertest');
+const app = require('./../src/app.js');
 
-const tape = require ('tape');
-const supertest = require ('supertest');
-const app = require ('./../src/app.js');
-//test the home route
+//test the / ( Home Page )route
 tape('test for home route ', (t) => {
-    supertest(app)
-      .get('/')
-      .expect(200)
-      .expect('Content-Type', /html/)
-      .end((err, res) => {
-        if (err) {
-          t.error(err);
-        }
-        t.ok(res.text.includes('body'));
-        t.equal(res.text.substr(0, 15), '<!DOCTYPE html>', 'the respone  should be html file');
-        t.equal(res.header['content-type'], 'text/html; charset=utf-8', 'the content-type should equal html');
-        t.end();
+  supertest(app)
+    .get('/')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end((err, res) => {
+      if (err)
+        t.error(err);
+      t.ok(res.text.includes('body'));
+      t.equal(res.text.substr(0, 15), '<!DOCTYPE html>', 'The Response Should Be Html Page');
+      t.end();
 
-      });
-  });
+    });
+});
 
+
+//test the /admin ( Admin Page ) route
 tape('check the admin page route', (t) => {
   supertest(app)
     .get('/admin')
     .expect(200)
     .expect('Content-Type', /html/)
     .end((err, res) => {
-      if (err) {
+      if (err)
         t.error(err);
-      }
       t.ok(res.text.includes('body'));
-      t.equal(res.text.substr(0, 15), '<!DOCTYPE html>', 'The response should be html');
+      t.equal(res.text.substr(0, 15), '<!DOCTYPE html>', 'The Response Should Be Html Page');
       t.end();
     });
 });
 
+//test the /article ( Article Page ) route
 tape('check the article page route', (t) => {
   supertest(app)
     .get('/article')
     .expect(200)
     .expect('Content-Type', /html/)
     .end((err, res) => {
-      if (err) {
+      if (err)
         t.error(err);
-      }
       t.ok(res.text.includes('body'));
-      t.equal(res.text.substr(0, 15), '<!DOCTYPE html>', 'The response should be html');
+      t.equal(res.text.substr(0, 15), '<!DOCTYPE html>', 'The Response Should Be Html Page');
       t.end();
     });
 });
