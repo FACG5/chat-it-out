8a6b981502837be1faac27064ce8d73495498f59
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-09-16 12:47:19 +0300

    Edit database #36

--- src/model/database/db_build.js
+++ src/model/database/db_build.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const dbConnection = require('./db_connection');
 
-const sql = fs.readFileSync(path.join(__dirname, 'db_build.js')).toString();
+const sql = fs.readFileSync(path.join(__dirname, 'db_build.sql')).toString();
 
 const dbBuild = cb => dbConnection.query(sql, (err, res) => {
   if (err) cb(err);
--- src/model/database/db_build.sql
+++ src/model/database/db_build.sql
@@ -1,36 +1,38 @@
 BEGIN;
 
+DROP TABLE IF EXISTS users,articles, suggestions,chats CASCADE;
 
-DROP TABLE IF EXISTS users,
-                     articles,
-                     suggestions,
-                     chats CASCADE;
+CREATE TABLE users (
+user_id SERIAL PRIMARY KEY,
+user_name VARCHAR(20) NOT NULL,
+user_email TEXT NOT NULL UNIQUE,
+user_password TEXT NOT NULL,
+permission VARCHAR(10) DEFAULT 'user'
+);
 
-
-CREATE TABLE users (user_id SERIAL PRIMARY KEY,
-                                           user_name VARCHAR(50) NOT NULL,
-                                                                 user_email TEXT NOT NULL UNIQUE,
-                                                                                          user_password TEXT NOT NULL,
-                                                                                                             permission VARCHAR(30) DEFAULT 'user');
-
-
-CREATE TABLE articles (article_id SERIAL PRIMARY KEY,
-                                                 article_title VARCHAR(250) NOT NULL,
-                                                                            article_img TEXT DEFAULT 'https://cached.imagescaler.hbpl.co.uk/resize/scaleWidth/614/cached.offlinehbpl.hbpl.co.uk/news/ORP/mentalhealth12-2016101210022372.jpg',
-                                                                                                     article_body TEXT NOT NULL);
+CREATE TABLE articles (
+article_id SERIAL PRIMARY KEY,
+article_title VARCHAR(100) NOT NULL,
+article_img TEXT DEFAULT 'https://cached.imagescaler.hbpl.co.uk/resize/scaleWidth/614/cached.offlinehbpl.hbpl.co.uk/news/ORP/mentalhealth12-2016101210022372.jpg',
+article_body TEXT NOT NULL
+);
 
 
-CREATE TABLE suggestions (suggestion_id SERIAL PRIMARY KEY,
-                                                 name VARCHAR(50) NOT NULL,
-                                                                  email TEXT NOT NULL,
-                                                                             subject TEXT NOT NULL,
-                                                                                          content TEXT NOT NULL);
+CREATE TABLE suggestions (
+suggestion_id SERIAL PRIMARY KEY,
+name VARCHAR(20) NOT NULL,
+email TEXT NOT NULL,
+subject TEXT NOT NULL,
+content TEXT NOT NULL
+);
 
 
-CREATE TABLE chats (id SERIAL PRIMARY KEY,
-                                      sender INTEGER REFERENCES users(user_id),
-                                                                receiver INTEGER REFERENCES users(user_id),
-                                                                                            message TEXT);
+CREATE TABLE chats (
+id SERIAL PRIMARY KEY,
+sender INTEGER REFERENCES users(user_id),
+receiver INTEGER REFERENCES users(user_id),
+message TEXT
+);
 
 
 COMMIT;
