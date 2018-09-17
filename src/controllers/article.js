a52cc9f7633ee77b9181d94cb8a549fcbbe90bc2
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-09-17 11:42:00 +0300

    solve hamburger problem

--- src/controllers/article.js
+++ src/controllers/article.js
@@ -1,5 +1,5 @@
 exports.get = (req, res) => {
   res.render('article', {
-    headerFound: true, footerFound: true, style: ['article', 'header', 'footer', 'public'], title: 'Article', javascript: ['hamburger'],
+    headerFound: true, footerFound: true, asideFound: true, style: ['article', 'header', 'footer', 'public'], title: 'Article', javascript: ['hamburger'],
   });
 };
