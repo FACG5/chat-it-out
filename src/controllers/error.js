26f94904566c3c014bf426df8fc3ee0cba8103ba
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-09-19 12:49:16 +0300

    Edits on the style #67

--- src/controllers/error.js
+++ src/controllers/error.js
@@ -1,24 +1,24 @@
 exports.clientError = (req, res) => {
-res.status(404).render('error', {
-    title: 'Error',
+  res.status(404).render('error', {
+    title: 'Error 404',
     headerFound: false,
     footerFound: false,
     asideFound: false,
-    style: ['public', 'error'],
+    style: ['error'],
     statusCode: '404',
     errorMsg: 'Page was not found !',
   });
 };
 
 exports.serverError = (err, req, res, next) => {
-res.status(500).render('error', {
-    title: 'Error',
+  res.status(500).render('error', {
+    title: 'Error 500',
     headerFound: false,
     footerFound: false,
     asideFound: false,
-    style: ['public', 'error'],
+    style: ['error'],
     statusCode: '500',
-    errorMsg: 'Internal Server Error'
+    errorMsg: 'Internal Server Error',
   });
   next();
 };
