32b8006caa4dc1fb7396bd1bfd8e4fe6d431d3d4
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-09-21 11:56:21 +0300

    Adding doctor.js file #75

---
+++ src/controllers/doctor.js
@@ -0,0 +1,16 @@
+const { getDoctor } = require('./../model/queries/getData');
+
+exports.get = (req, res) => {
+  getDoctor(req.params.id).then((result) => {
+    res.render('doctor', {
+      title: 'Doctor ',
+      headerFound: true,
+      footerFound: true,
+      style: ['public', 'header', 'footer', 'doctor'],
+      javascript: ['hamburger'],
+      doctor: result.rows[0],
+      login: (res.locals.unlockCookie === null),
+      username: (res.locals.unlockCookie === null) ? 'Unkown' : (res.locals.unlockCookie.username),
+    });
+  });
+};
