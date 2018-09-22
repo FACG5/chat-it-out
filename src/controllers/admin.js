022d9493eb36c79303f807faecc51795cb5a8c27
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-09-22 11:08:08 +0300

    solve the issues #75

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,7 +1,7 @@
 const { insertArticle } = require('../model/queries/addData');
 
 exports.get = (req, res) => {
-  if (res.locals.unlockCookie.permission === 'admin') {
+  if (res.locals.unlockCookie && res.locals.unlockCookie.permission === 'admin') {
     res.render('admin', {
       title: 'Admin Panel', headerFound: true, footerFound: true, asideFound: true, style: ['admin', 'header', 'footer'], javascript: ['hamburger', 'admin'], login: (res.locals.unlockCookie === null), username: (res.locals.unlockCookie === null) ? 'Unkown' : res.locals.unlockCookie.username,
     });
