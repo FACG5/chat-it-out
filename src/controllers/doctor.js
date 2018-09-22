95a486a0cf1ed4534ae2a0da2581b4e8f2f3f4c5
Mohanned <mohannad.hanafi.b@hotmail.com>
2018-09-22 11:19:19 +0300

    solve issues #72

--- src/controllers/doctor.js
+++ src/controllers/doctor.js
@@ -1,16 +1,20 @@
 const { getDoctor } = require('./../model/queries/getData');
 
-exports.get = (req, res) => {
+exports.get = (req, res, next) => {
   getDoctor(req.params.id).then((result) => {
-    res.render('doctor', {
-      title: 'Doctor ',
-      headerFound: true,
-      footerFound: true,
-      style: ['public', 'header', 'footer', 'doctor'],
-      javascript: ['hamburger'],
-      doctor: result.rows[0],
-      login: (res.locals.unlockCookie === null),
-      username: (res.locals.unlockCookie === null) ? 'Unkown' : (res.locals.unlockCookie.username),
-    });
+    if (result.rowCount !== 0) {
+      res.render('doctor', {
+        title: 'Doctor ',
+        headerFound: true,
+        footerFound: true,
+        style: ['public', 'header', 'footer', 'doctor'],
+        javascript: ['hamburger'],
+        doctor: result.rows[0],
+        login: (res.locals.unlockCookie === null),
+        username: (res.locals.unlockCookie === null) ? 'Unkown' : (res.locals.unlockCookie.username),
+      });
+    } else {
+      next();
+    }
   });
 };
